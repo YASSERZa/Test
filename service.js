f3ad6389ee886f4a942e7cc1863293559a812386
yasralzhrany <yasr.alzhrany@student.fairfield.edu>
2022-12-08 23:33:40 -0500

    deploy to heroku

--- service.js
+++ service.js
@@ -7,6 +7,9 @@ app.use(express.static(__dirname + '/dist/routing-demo'));
 app.get('/*', function(req,res) {    
 res.sendFile(path.join(__dirname+'/dist/routing-demo/index.html'));
 });
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
+
 
 
 
